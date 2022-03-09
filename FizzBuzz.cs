b2fea5ddce47b7c2fd109defb1ebac8c0978c9b9
phurise <phurise01@gmail.com>
2022-03-09 12:50:56 -0500

    just some fizzbuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,45 @@
+using Internal;
+using System;
+
+class ConsoleUI
+{
+    static void Main(string[] args)
+    {
+        
+        int number = 0;
+        bool parsed = false;
+
+        Console.Write("Enter your number: ");
+        do
+        {
+            string userInput = Console.ReadLine();
+            parsed = int.TryParse(userInput, out number); 
+
+            if (!parsed)
+            {
+                Console.WriteLine("Input invalid. Try Again.");
+            }
+
+        } while (!parsed);
+
+        Console.Clear();
+        //if divisible by 3 print fizz
+        if (number % 3 == 0 && number % 5 != 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (number % 3 != 0 && number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else if (number % 3 == 0 && number % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else
+        {
+            Console.WriteLine("Something happened.");
+        }
+        
+    }
+}
